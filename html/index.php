321c54f1871f56b94e5a7b8a4d884c99db585d56
Todd Zaki Warfel <zakiwarfel@me.com>
2012-01-19 09:40:31 -0500

    Updates to styles, media queries, js files for additional mobile work.
    Signed-off-by: Todd Zaki Warfel <zakiwarfel@me.com>

---
+++ html/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<?php include 'lib/partials/_meta.php' ?>
+	<?php include 'lib/partials/_scripts.php' ?>
+	<title>Rapleaf Demo</title>
+</head>
+<body class="dashboard">
+<div id="StickyWrap" class="wrapper">
+	<div id="HeadNav">
+		<?php include 'lib/partials/_nav.php' ?>
+	</div>
+	<article id="Main">
+		<section>
+			<h1>This is an h1</h1>
+			<h2>This is an h2</h2>
+			<h3>This is an h3</h3>
+			<h4>This is an h4</h4>
+			<h5>This is an h5</h5>
+			<h6>This is an h6</h6>
+			<hr>
+			<p>
+				Example of paragraph text.
+			</p>
+			<ul>
+				<li>List item one</li>
+				<li>List item two</li>
+				<li>List item three</li>
+				<li>List item four</li>
+				<li>List item five</li>
+			</ul>
+			<code>Example source code</code>
+			<hr>
+			<form action="" method="" accept-charset="utf-8">
+			<fieldset id="sample_form" class="">
+				<legend>Example form</legend>
+				<ol>
+					<li><input type="text" name="Name" placeholder="Enter your full name" class="placeholder" required></li>
+					<li><input type="email" name="Email" placeholder="Enter your email address" class="placeholder" required></li>
+					<li><input type="url" name="URL" placeholder="Enter your web address" class="placeholder" required></li>
+					<li><input type="tel" name="Tel" placeholder="+1 123 456 7890" class="placeholder" required></li>
+					<li><input type="file"></li>
+					<li><input type="submit" name="Send" value="Send it" class="btn"> <span class="quiet">or</span> <a href="#" class="admin">Cancel, don't send</a></li>
+				</ol>
+			</fieldset>
+			</form>
+			<hr>
+			<table cellspacing="0">
+				<tr>
+					<th>Header</th>
+					<th>Header</th>
+					<th>Header</th>
+				</tr>
+				<tr>
+					<td>Data</td>
+					<td>Data</td>
+					<td>Data</td>
+				</tr>
+				<tr class="odd">
+					<td>Data</td>
+					<td>Data</td>
+					<td>Data</td>
+				</tr>
+				<tr>
+					<td>Data</td>
+					<td>Data</td>
+					<td>Data</td>
+				</tr>
+			</table>
+		</section>
+	</article>
+</div>
+<?php include 'lib/partials/_foot.php' ?>
+</body>
+</html>
--- html/lib/js/base.js
+++ html/lib/js/base.js
@@ -1,26 +1,58 @@
 // Core global JS functions
 $(function() {
-
+	
 // Global initializations
 	$(document).ready(function() {
 		$('.toggle').hide(); // Global hide function. Use this to hide anything on the page when the document loads.
-		
 		$('input, textarea').placeholder(); // Sets up placeholder for browsers w/o native support
-			});
-			
-// Start custom JavaScript here	
-		
-	
-/*		$('.flash').click(function() { 
-				$(this).slideUp("fast");
- 		});
- 	
- 	$('.flash').fadeIn( function() { // Fade out after 5 seconds
- 		  setTimeout( function() {
- 		  	$(".success").fadeOut("fast");
- 			}, 5000);
- 		});*/
-			
 	});
 	
+	$('.flash').click(function() { 
+		$(this).slideUp('fast');
+	});
+	
+	$('.flash').fadeIn( function() {
+		  setTimeout( function() {
+		  	$(".flash.success").fadeOut("fast");
+		}, 5000);
+	});
+		
+});
+
+$(function() {
+
+	// Auto scroll to hide URL bar on mobile browsers
+	// When ready...
+	window.addEventListener('load',function() {
+	  // Set a timeout...
+	  setTimeout(function(){
+	    // Hide the address bar!
+	    window.scrollTo(0, 1);
+	  }, 0);
+	});
+	
+	// Watch for change between portrait and landscape modes
+	addEventListener('load', function()
+	{
+	    setTimeout(updateLayout, 0);
+	}, false);
+
+	var currentWidth = 0;
+
+	function updateLayout()
+	{
+	    if (window.innerWidth != currentWidth)
+	    {
+	        currentWidth = window.innerWidth;
+
+	        var orient = currentWidth == 320 ? 'profile' : 'landscape';
+	        document.body.setAttribute('orient', orient);
+	        setTimeout(function()
+	        {
+	            window.scrollTo(0, 1);
+	        }, 100);           
+	    }
+	}
+
+	setInterval(updateLayout, 400);
 });
